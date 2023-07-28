53da5d87de96b183712f5940afad23021090e300
srmojumdar <rimonmojumdar88@gmail.com>
2023-07-28 13:38:29 +0600

    Added to git hub

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      themes: ["light", "dark", "cupcake","retro"],
+      extend: {},
+    },
+    plugins: [],
+  }
